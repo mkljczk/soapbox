36cc2e103ae124183315bd4ac6d3ab05a1d7dbce
marcin mikołajczak <git@mkljczk.pl>
2024-10-26 19:53:26 +0200

    pl-hooks: Improve useInstance behavior
    Signed-off-by: marcin mikołajczak <git@mkljczk.pl>

--- packages/pl-hooks/lib/hooks/instance/use-instance.ts
+++ packages/pl-hooks/lib/hooks/instance/use-instance.ts
@@ -5,7 +5,7 @@ import * as v from 'valibot';
 import { usePlHooksApiClient } from 'pl-hooks/contexts/api-client';
 import { usePlHooksQueryClient } from 'pl-hooks/contexts/query-client';
 
-const placeholderData = v.parse(instanceSchema, {});
+const initialData = v.parse(instanceSchema, {});
 
 const useInstance = () => {
   const { client } = usePlHooksApiClient();
@@ -14,7 +14,8 @@ const useInstance = () => {
   return useQuery({
     queryKey: ['instance'],
     queryFn: client.instance.getInstance,
-    placeholderData,
+    initialData,
+    initialDataUpdatedAt: -1,
   }, queryClient);
 };
 
